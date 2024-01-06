21a0a03c85baac3fd3ead438aaf26c48cdd39603
Mustafa Shujaie <mfshujaie@gmail.com>
2024-01-06 23:01:02 +0400

    update next config

--- next.config.js
+++ next.config.js
@@ -1,8 +1,10 @@
-/** @type {import('next').NextConfig} */
+const protocol = process.env.NODE_ENV === "development" ? "http" : "https";
+
 const nextConfig = {
   reactStrictMode: true,
   env: {
     NEXT_PUBLIC_BACKEND_URL: process.env.NEXT_PUBLIC_BACKEND_URL,
+    NEXT_PUBLIC_BACKEND_URL: `${protocol}://${process.env.BACKEND_HOST}`,
   },
 };
 
