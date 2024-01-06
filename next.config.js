323a957daa2b7f11e52f0a31393c8e6302499f77
Mustafa Shujaie <mfshujaie@gmail.com>
2024-01-06 23:20:58 +0400

    revert next config

--- next.config.js
+++ next.config.js
@@ -2,9 +2,6 @@ const nextConfig = {
   reactStrictMode: true,
   env: {
     NEXT_PUBLIC_BACKEND_URL: process.env.NEXT_PUBLIC_BACKEND_URL,
-    NEXT_PUBLIC_BACKEND_URL: `${process.env.NODE_ENV === "development" ? "http" : "https"}://${
-      process.env.BACKEND_HOST
-    }`,
   },
 };
 
