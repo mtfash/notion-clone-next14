40ff4cd92f68adc824afba702f220a953a484f7f
Mustafa Shujaie <mfshujaie@gmail.com>
2024-01-06 23:14:25 +0400

    log next config

--- next.config.js
+++ next.config.js
@@ -1,11 +1,13 @@
-const protocol = process.env.NODE_ENV === "development" ? "http" : "https";
-
 const nextConfig = {
   reactStrictMode: true,
   env: {
     NEXT_PUBLIC_BACKEND_URL: process.env.NEXT_PUBLIC_BACKEND_URL,
-    NEXT_PUBLIC_BACKEND_URL: `${protocol}://${process.env.BACKEND_HOST}`,
+    NEXT_PUBLIC_BACKEND_URL: `${process.env.NODE_ENV === "development" ? "http" : "https"}://${
+      process.env.BACKEND_HOST
+    }`,
   },
 };
+
+console.log(nextConfig);
 
 module.exports = nextConfig;
